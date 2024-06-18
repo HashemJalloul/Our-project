0feef4caadb6c17829adf02b637414232b3a1452
esraa-kar23 <esraaka247@gmail.com>
2024-06-18 03:43:24 +0300

    ek-card-category

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { createBrowserRouter, RouterProvider, } from "react-router-dom";
-// import Home from './pages/Home/Home.jsx';
+import Home from './pages/Home/Home.jsx';
 
 
 const router = createBrowserRouter([
@@ -12,14 +12,14 @@ const router = createBrowserRouter([
     path: "/",
     element: <App />,
 
-    // children: [
-    //   {
-    //  path: "/",
-    //  element: <Home />,
-    // },
+    children: [
+    {
+     path: "/Home",
+     element: <Home />,
+    },
    
 
-    // ]
+    ]
 
 
   },
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,19 @@
 import './Home.css'
+import Category from '../../components/Category/Category'
+
 
 export default function Home() {
   return (
-    <section className='ek-hero'>
-        
-    </section>
+    <>
+      <section className='ek-hero'>
+        <div className="ek-hero-content">
+          <h1 className='ek-hero-title'>Build Skills with
+            Online Course</h1>
+          <p className='ek-p-hero'>We denounce with righteous indignation and dislike men who are so beguiled and demoralized that cannot trouble.</p>
+          <button className='ek-btn-hero'><a href="">Posts comment</a></button>
+        </div>
+      </section>
+      <Category />
+    </>
   )
 }
