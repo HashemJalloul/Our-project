638577035a8a140a951361ff87c9979c80983332
MhdAliAjaj <engmhdsy630@gmail.com>
2024-07-02 23:25:39 +0300

    CourseListing SideBar  resposive

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,12 +1,23 @@
 import "./Sidebar.css";
-import stary from './../../../public/imges/stary.png'
-import starg from './../../../public/imges/starg.png'
+import stary from "./../../../public/imges/stary.png";
+import starg from "./../../../public/imges/starg.png";
 const Sidebar = () => {
+  document.addEventListener("DOMContentLoaded", function() {
+    const toggleButton = document.getElementById("toggle-button");
+    const sidebar = document.getElementById("MA-courses-sidebar");
+
+    toggleButton.addEventListener("click", function() {
+        sidebar.classList.toggle("open");
+    });
+});
   return (
-    <div className="MA-courses-sidebar">
-      
+    <div className="MA-courses-sidebar" id="MA-courses-sidebar">
+      <button class="toggle-button" id="toggle-button">
+        <div style={{border:"1px solid #665d5d",cursor:"pointer",borderRadius:"4px"}}>☰</div>
+      </button>
+      <div class="MA-sidebar-content" id="MA-sidebar-content">
       <div className="MA-sidebar-course-category">
-      <h4>Course Category</h4>
+        <h4>Course Category</h4>
         <div className="MA-side-category">
           <div className="MA-side-1">
             <input type="checkbox" />
@@ -15,85 +26,77 @@ const Sidebar = () => {
           <p>15</p>
         </div>
 
+        <div className="MA-side-category">
+          <div className="MA-side-1">
+            <input type="checkbox" />
+            <p>Office</p>
+          </div>
+          <p>15</p>
+        </div>
 
         <div className="MA-side-category">
-        <div className="MA-side-1">
-          <input type="checkbox" /> 
-          <p>Office</p>
+          <div className="MA-side-1">
+            <input type="checkbox" />
+            <p>Shop</p>
+          </div>
+          <p>15</p>
         </div>
-        <p>15</p>
-        </div>
-
-
 
         <div className="MA-side-category">
-        <div className="MA-side-1">
-          <input type="checkbox" /> 
-          <p>Shop</p>
+          <div className="MA-side-1">
+            <input type="checkbox" />
+            <p>Education</p>
+          </div>
+          <p>15</p>
         </div>
-        <p>15</p>
-        </div>
-
 
         <div className="MA-side-category">
-        <div className="MA-side-1">
-          <input type="checkbox" /> 
-          <p>Education</p>
+          <div className="MA-side-1">
+            <input type="checkbox" />
+            <p>Academy</p>
+          </div>
+          <p>15</p>
         </div>
-        <p>15</p>
-        </div>
-
 
         <div className="MA-side-category">
-        <div className="MA-side-1">
-          <input type="checkbox" /> 
-          <p>Academy</p>
+          <div className="MA-side-1">
+            <input type="checkbox" />
+            <p>Single family home</p>
+          </div>
+          <p>15</p>
         </div>
-        <p>15</p>
-        </div>
-
 
         <div className="MA-side-category">
-        <div className="MA-side-1">
-          <input type="checkbox" /> 
-          <p>Single family home</p>
+          <div className="MA-side-1">
+            <input type="checkbox" />
+            <p>Studio</p>
+          </div>
+          <p>15</p>
         </div>
-        <p>15</p>
-        </div>
-
 
         <div className="MA-side-category">
-        <div className="MA-side-1">
-          <input type="checkbox" /> 
-          <p>Studio</p>
+          <div className="MA-side-1">
+            <input type="checkbox" />
+            <p>University</p>
+          </div>
+          <p>15</p>
         </div>
-        <p>15</p>
-        </div>
-
-
-        <div className="MA-side-category">
-        <div className="MA-side-1">
-          <input type="checkbox" /> 
-          <p>University</p>
-        </div>
-        <p>15</p>
-        </div>
-        </div>
+      </div>
 
       <div className="MA-sidebar-Instructors">
         <h4>Instructors</h4>
 
         <div className="MA-side-category">
           <div className="MA-side-1">
-          <input type="checkbox" />
-          <p>Jenny Wilson</p>
+            <input type="checkbox" />
+            <p>Jenny Wilson</p>
           </div>
           <p>15</p>
         </div>
         <div className="MA-side-category">
           <div className="MA-side-1">
-          <input type="checkbox" />
-          <p>John Doe</p>
+            <input type="checkbox" />
+            <p>John Doe</p>
           </div>
           <p>15</p>
         </div>
@@ -103,132 +106,134 @@ const Sidebar = () => {
         <h4>Price</h4>
 
         <div className="MA-side-category">
-        <div className="MA-side-1">
-        <input type="checkbox" /> 
-        <p>All</p>
-        </div>
-        <p>15</p>
-        </div>
-
-        <div  className="MA-side-category">
-         <div className="MA-side-1">
-         <input type="checkbox" /> 
-         <p>Free</p>
-         </div>
-         <p>15</p>
+          <div className="MA-side-1">
+            <input type="checkbox" />
+            <p>All</p>
+          </div>
+          <p>15</p>
         </div>
 
         <div className="MA-side-category">
-         <div className="MA-side-1">
-         <input type="checkbox" />
-         <p>Paid</p>
-         </div>
-         <p>15</p>
+          <div className="MA-side-1">
+            <input type="checkbox" />
+            <p>Free</p>
+          </div>
+          <p>15</p>
+        </div>
+
+        <div className="MA-side-category">
+          <div className="MA-side-1">
+            <input type="checkbox" />
+            <p>Paid</p>
+          </div>
+          <p>15</p>
         </div>
       </div>
 
       <div className="MA-sidebar-Review">
         <h4>Review</h4>
         <div className="MA-side-category">
-         <div className="MA-side-1-star">
-         <input type="checkbox" /> 
-         <div className="star">
-         <img src={stary} alt="" />
-         <img src={stary} alt="" />
-         <img src={stary} alt="" />
-         <img src={stary} alt="" />
-         <img src={stary} alt="" />
-         </div>
-         </div>
+          <div className="MA-side-1-star">
+            <input type="checkbox" />
+            <div className="star">
+              <img src={stary} alt="" />
+              <img src={stary} alt="" />
+              <img src={stary} alt="" />
+              <img src={stary} alt="" />
+              <img src={stary} alt="" />
+            </div>
+          </div>
           <p>(1,025)</p>
         </div>
         <div className="MA-side-category">
-         <div className="MA-side-1-star">
-         <input type="checkbox" /> 
-         <div className="star">
-         <img src={stary} alt="" />
-         <img src={stary} alt="" />
-         <img src={stary} alt="" />
-         <img src={stary} alt="" />
-         <img src={starg} alt="" />
-         </div>
-         </div>
+          <div className="MA-side-1-star">
+            <input type="checkbox" />
+            <div className="star">
+              <img src={stary} alt="" />
+              <img src={stary} alt="" />
+              <img src={stary} alt="" />
+              <img src={stary} alt="" />
+              <img src={starg} alt="" />
+            </div>
+          </div>
           <p>(1,025)</p>
         </div>
         <div className="MA-side-category">
-         <div className="MA-side-1-star">
-         <input type="checkbox" /> 
-         <div className="star">
-         <img src={stary} alt="" />
-         <img src={stary} alt="" />
-         <img src={stary} alt="" />
-         <img src={starg} alt="" />
-         <img src={starg} alt="" />
-         </div>
-         </div>
+          <div className="MA-side-1-star">
+            <input type="checkbox" />
+            <div className="star">
+              <img src={stary} alt="" />
+              <img src={stary} alt="" />
+              <img src={stary} alt="" />
+              <img src={starg} alt="" />
+              <img src={starg} alt="" />
+            </div>
+          </div>
           <p>(1,025)</p>
         </div>
         <div className="MA-side-category">
-         <div className="MA-side-1-star">
-         <input type="checkbox" /> 
-        <div className="star">
-        <img src={stary} alt="" />
-         <img src={stary} alt="" />
-         <img src={starg} alt="" />
-         <img src={starg} alt="" />
-         <img src={starg} alt="" />
-        </div>
-         </div>
+          <div className="MA-side-1-star">
+            <input type="checkbox" />
+            <div className="star">
+              <img src={stary} alt="" />
+              <img src={stary} alt="" />
+              <img src={starg} alt="" />
+              <img src={starg} alt="" />
+              <img src={starg} alt="" />
+            </div>
+          </div>
           <p>(1,025)</p>
         </div>
         <div className="MA-side-category">
-         <div className="MA-side-1-star">
-         <input type="checkbox" /> 
-         <div className="star">
-         <img src={stary} alt="" />
-         <img src={starg} alt="" />
-         <img src={starg} alt="" />
-         <img src={starg} alt="" />
-         <img src={starg} alt="" />
-         </div>
-         </div>
+          <div className="MA-side-1-star">
+            <input type="checkbox" />
+            <div className="star">
+              <img src={stary} alt="" />
+              <img src={starg} alt="" />
+              <img src={starg} alt="" />
+              <img src={starg} alt="" />
+              <img src={starg} alt="" />
+            </div>
+          </div>
           <p>(1,025)</p>
         </div>
-        
       </div>
 
       <div className="MA-sidebar-Level">
         <h4>Level</h4>
 
         <div className="MA-side-category">
-         <div className="MA-side-1">
-         <input type="checkbox" />
-         <p>All levels</p>
-         </div>
-         <p>15</p>
+          <div className="MA-side-1">
+            <input type="checkbox" />
+            <p>All levels</p>
+          </div>
+          <p>15</p>
         </div>
         <label htmlFor="" className="MA-side-category">
-         <div className="MA-side-1">
-         <input type="checkbox" />
-         <p>Beginner</p>
-         </div>
-         <p>15</p>
+          <div className="MA-side-1">
+            <input type="checkbox" />
+            <p>Beginner</p>
+          </div>
+          <p>15</p>
         </label>
         <div className="MA-side-category">
-         <div className="MA-side-1">
-         <input type="checkbox" />
-         <p>Intermidiate</p>
-         </div>
-         <p>15</p>
+          <div className="MA-side-1">
+            <input type="checkbox" />
+            <p>Intermidiate</p>
+          </div>
+          <p>15</p>
         </div>
         <div className="MA-side-category">
-         <div className="MA-side-1">
-         <input type="checkbox" />
-         <p>Expert</p>
-         </div>
-         <p>15</p>
+          <div className="MA-side-1">
+            <input type="checkbox" />
+            <p>Expert</p>
+          </div>
+          <p>15</p>
         </div>
       </div>
+
+      </div>
+     
     </div>
   );
 };
