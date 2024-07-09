2a27f4c7bf3cb1c9a4d36959e1220638d3a7b65c
HashemJalloul <jalloulhashem@gmail.com>
2024-07-09 22:57:51 +0400

    update

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { createBrowserRouter, RouterProvider, } from "react-router-dom";
-import FAQS from './pages/FAQS/FAQS.jsx';
 import Home from './pages/Home/Home.jsx';
 import CourseListing from './pages/CourseListing/CourseListing.jsx'
 
@@ -15,10 +14,7 @@ const router = createBrowserRouter([
     element: <App />,
 
     children: [
-      {
-     path: "/FAQS",
-     element: <FAQS />,
-    },
+  
    
     {
      path: "/Home",
